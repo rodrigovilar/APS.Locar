286c1b21bf44dd20b5de7a4dadfe0d0cf8d9627f
NEEMIAS <NEEMIAS@NEEMIAS-PC>
2013-09-24 19:45:41 -0300

    Correção do junit

--- src/br/ufpb/aps/locar/TesteLocadoraDeVeiculos.java
+++ src/br/ufpb/aps/locar/TesteLocadoraDeVeiculos.java
@@ -1,6 +1,8 @@
 package br.ufpb.aps.locar;
 
 import static org.junit.Assert.*;
+
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
